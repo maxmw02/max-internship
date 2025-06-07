c789cd8e013686570285d9a7d14e5c4a5a1c1744
Max <mwmaxoz@gmail.com>
2025-06-07 13:14:00 -0400

    increased array for skeleton loading state items

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -80,7 +80,7 @@ const ExploreItems = () => {
       </div>
       {loading && (
         <>
-          {new Array(4).fill(0).map((_, index) => (
+          {new Array(8).fill(0).map((_, index) => (
             <div
               key={index}
               className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
